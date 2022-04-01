f1d4b632e2f303781fa65113f865e9fbb359c044
Anurag <2006535@kiit.ac.in>
2022-04-01 08:06:04 +0530

    overlay!

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,11 +2,8 @@ const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-
 const navItems = document.querySelectorAll('.nav-item');
-
 let showMenu = false;
-
 menuBtn.addEventListener('click', toggleMenu);
 function toggleMenu() {
     if(!showMenu){
@@ -18,12 +15,10 @@ function toggleMenu() {
         showMenu=true;
     }
     else{
-
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
-        menuBranding.classList.remove('show');
-        
+        menuBranding.classList.remove('show');       
         navItems.forEach(item => item.classList.remove('show'));
         showMenu=false;
     }
